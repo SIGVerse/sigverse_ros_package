d7e97d8b12979aa167cc47642bf3db3539f3a2a6
Noriyuki-Kugou <phenom7kugo@gmail.com>
2019-01-25 15:54:57 +0900

    Fix to use isContainJointNames function.

--- samples/hsr/hsr_joint_state_2_joint_trajectory/src/hsr_joint_state_2_joint_trajectory.cpp
+++ samples/hsr/hsr_joint_state_2_joint_trajectory/src/hsr_joint_state_2_joint_trajectory.cpp
@@ -17,13 +17,17 @@ private:
 
   static void rosSigintHandler(int sig);
   void jointStateCallback(const sensor_msgs::JointState::ConstPtr& joint_state);
+  bool isContainJointNames(std::vector<std::string> joint_names);
   double getCurrentJointStatesAngle(std::string joint_name);
   void moveArm();
   void moveHead();
   void moveGripper();
 
-  std::map<std::string, double> default_joint_angle_;
   sensor_msgs::JointState current_joint_states_;
+
+  std::vector<std::string> arm_joint_names_;
+  std::vector<std::string> head_joint_names_;
+  std::vector<std::string> gripper_joint_names_;
 
   ros::NodeHandle node_handle_;
   ros::Subscriber sub_joint_state_;
@@ -39,14 +43,14 @@ HsrJointState2JointTrajectory::HsrJointState2JointTrajectory()
   // This must be set after the first NodeHandle is created.
   signal(SIGINT, rosSigintHandler);
 
-  default_joint_angle_["arm_lift_joint"]   = 0.0;
-  default_joint_angle_["arm_flex_joint"]   = 0.0;
-  default_joint_angle_["arm_roll_joint"]   = 0.0;
-  default_joint_angle_["wrist_flex_joint"] = 0.0;
-  default_joint_angle_["wrist_roll_joint"] = 0.0;
-  default_joint_angle_["head_pan_joint"]   = 0.0;
-  default_joint_angle_["head_tilt_joint"]  = 0.0;
-  default_joint_angle_["hand_motor_joint"] = 0.0;
+  arm_joint_names_.push_back("arm_lift_joint");
+  arm_joint_names_.push_back("arm_flex_joint");
+  arm_joint_names_.push_back("arm_roll_joint");
+  arm_joint_names_.push_back("wrist_flex_joint");
+  arm_joint_names_.push_back("wrist_roll_joint");
+  head_joint_names_.push_back("head_pan_joint");
+  head_joint_names_.push_back("head_tilt_joint");
+  gripper_joint_names_.push_back("hand_motor_joint");
 
   std::string sub_joint_state_topic_name;
   std::string pub_arm_trajectory_topic_name;
@@ -77,45 +81,44 @@ void HsrJointState2JointTrajectory::jointStateCallback(const sensor_msgs::JointS
 }
 
 
-double HsrJointState2JointTrajectory::getCurrentJointStatesAngle(std::string joint_name)
+bool HsrJointState2JointTrajectory::isContainJointNames(std::vector<std::string> joint_names)
 {
-  if(current_joint_states_.position.empty())
+  if(current_joint_states_.name.empty()){ return false; }
+
+  for(int i = 0; i < joint_names.size(); i++)
   {
-    return default_joint_angle_[joint_name];
+    std::vector<std::string>::iterator it =std::find(current_joint_states_.name.begin(), current_joint_states_.name.end(), joint_names[i]);
+    if(it == current_joint_states_.name.end()){ return false; }
   }
 
+  return true;
+}
+
+
+double HsrJointState2JointTrajectory::getCurrentJointStatesAngle(std::string joint_name)
+{
   std::vector<std::string>::iterator it =std::find(current_joint_states_.name.begin(), current_joint_states_.name.end(), joint_name);
-  if (it != current_joint_states_.name.end())
-  {
-    int index = std::distance(current_joint_states_.name.begin(), it);
-    return current_joint_states_.position[index];
-  }
-  else
-  {
-    return default_joint_angle_[joint_name];
-  }
+  int index = std::distance(current_joint_states_.name.begin(), it);
+  return current_joint_states_.position[index];
 }
 
 
 void HsrJointState2JointTrajectory::moveArm()
 {
+  if(isContainJointNames(arm_joint_names_) == false){ return; }
+
   std::vector<double> goal_position;
-  goal_position.push_back(getCurrentJointStatesAngle("arm_lift_joint"));
-  goal_position.push_back(getCurrentJointStatesAngle("arm_flex_joint"));
-  goal_position.push_back(getCurrentJointStatesAngle("arm_roll_joint"));
-  goal_position.push_back(getCurrentJointStatesAngle("wrist_flex_joint"));
-  goal_position.push_back(getCurrentJointStatesAngle("wrist_roll_joint"));
+
+  for(int i = 0; i < arm_joint_names_.size(); i++){
+    goal_position.push_back(getCurrentJointStatesAngle(arm_joint_names_[i]));
+  }
 
   trajectory_msgs::JointTrajectoryPoint arm_joint_point;
   arm_joint_point.positions = goal_position;
   arm_joint_point.time_from_start = ros::Duration(0.5);
 
   trajectory_msgs::JointTrajectory joint_trajectory;
-  joint_trajectory.joint_names.push_back("arm_lift_joint");
-  joint_trajectory.joint_names.push_back("arm_flex_joint");
-  joint_trajectory.joint_names.push_back("arm_roll_joint");
-  joint_trajectory.joint_names.push_back("wrist_flex_joint");
-  joint_trajectory.joint_names.push_back("wrist_roll_joint");
+  joint_trajectory.joint_names = arm_joint_names_;
   joint_trajectory.points.push_back(arm_joint_point);
 
   pub_arm_trajectory_.publish(joint_trajectory);
@@ -124,17 +127,20 @@ void HsrJointState2JointTrajectory::moveArm()
 
 void HsrJointState2JointTrajectory::moveHead()
 {
+  if(isContainJointNames(head_joint_names_) == false){ return; }
+
   std::vector<double> goal_position;
-  goal_position.push_back(getCurrentJointStatesAngle("head_pan_joint"));
-  goal_position.push_back(getCurrentJointStatesAngle("head_tilt_joint"));
+
+  for(int i = 0; i < head_joint_names_.size(); i++){
+    goal_position.push_back(getCurrentJointStatesAngle(head_joint_names_[i]));
+  }
 
   trajectory_msgs::JointTrajectoryPoint head_joint_point;
   head_joint_point.positions = goal_position;
   head_joint_point.time_from_start = ros::Duration(0.5);
 
   trajectory_msgs::JointTrajectory joint_trajectory;
-  joint_trajectory.joint_names.push_back("head_pan_joint");
-  joint_trajectory.joint_names.push_back("head_tilt_joint");
+  joint_trajectory.joint_names = head_joint_names_;
   joint_trajectory.points.push_back(head_joint_point);
 
   pub_head_trajectory_.publish(joint_trajectory);
@@ -143,15 +149,20 @@ void HsrJointState2JointTrajectory::moveHead()
 
 void HsrJointState2JointTrajectory::moveGripper()
 {
+  if(isContainJointNames(gripper_joint_names_) == false){ return; }
+
   std::vector<double> goal_position;
-  goal_position.push_back(getCurrentJointStatesAngle("hand_motor_joint"));
+
+  for(int i = 0; i < gripper_joint_names_.size(); i++){
+    goal_position.push_back(getCurrentJointStatesAngle(gripper_joint_names_[i]));
+  }
 
   trajectory_msgs::JointTrajectoryPoint gripper_joint_point;
   gripper_joint_point.positions = goal_position;
   gripper_joint_point.time_from_start = ros::Duration(0.5);
 
   trajectory_msgs::JointTrajectory joint_trajectory;
-  joint_trajectory.joint_names.push_back("hand_motor_joint");
+  joint_trajectory.joint_names = gripper_joint_names_;
   joint_trajectory.points.push_back(gripper_joint_point);
 
   pub_gripper_trajectory_.publish(joint_trajectory);
