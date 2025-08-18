e7148ee7f95bdfa9f6d27ce396e81983e64aafb0
H.yamada <yamada4tec@gmail.com>
2025-08-18 18:28:29 +0900

    Ported TurtleBot3 slam package to ROS2

---
+++ examples/turtlebot3/laser_distance_sensor/README.md
@@ -0,0 +1,19 @@
+# sigverse_turtlebot3_laser_distance_sensor
+
+Keyboard teleoperation for TurtleBot3. 
+
+## Prerequisites
+
+- **slam-toolbox**
+
+For reference
+```bash
+$ sudo apt install ros-$ROS_DISTRO-slam-toolbox
+```
+
+## How To Use
+
+```bash
+$ ros2 launch sigverse_turtlebot3_laser_distance_sensor slam.launch
+```
+
--- examples/turtlebot3/laser_distance_sensor/src/teleop_key.cpp
+++ examples/turtlebot3/laser_distance_sensor/src/teleop_key.cpp
@@ -1,14 +1,18 @@
+
 #include <cstdio>
 #include <csignal>
 #include <unistd.h>
 #include <termios.h>
-#include <ros/ros.h>
-#include <geometry_msgs/Twist.h>
+#include <memory>
+#include <functional>
+#include <cstring>
+
+#include "rclcpp/rclcpp.hpp"
+#include "geometry_msgs/msg/twist.hpp"
 
 class SIGVerseTb3LaserDistanceSensorTeleopKey
 {
 private:
-
   static const char KEYCODE_UP    = 0x41;
   static const char KEYCODE_DOWN  = 0x42;
   static const char KEYCODE_RIGHT = 0x43;
@@ -31,46 +35,39 @@ public:
   void keyLoop(int argc, char** argv);
 
 private:
-
   static void rosSigintHandler(int sig);
-  static int  canReceiveKey( const int fd );
+  static int  canReceiveKey(const int fd);
 
-  void moveBase(ros::Publisher &publisher, const double linear_x, const double angular_z);
+  void moveBase(rclcpp::Publisher<geometry_msgs::msg::Twist>::SharedPtr &publisher, const double linear_x, const double angular_z);
 
   void showHelp();
 };
-
 
 SIGVerseTb3LaserDistanceSensorTeleopKey::SIGVerseTb3LaserDistanceSensorTeleopKey()
 {
 }
 
-
-void SIGVerseTb3LaserDistanceSensorTeleopKey::rosSigintHandler(int sig)
+void SIGVerseTb3LaserDistanceSensorTeleopKey::rosSigintHandler([[maybe_unused]] int sig)
 {
-  ros::shutdown();
+  rclcpp::shutdown();
 }
 
-
-int SIGVerseTb3LaserDistanceSensorTeleopKey::canReceiveKey( const int fd )
+int SIGVerseTb3LaserDistanceSensorTeleopKey::canReceiveKey(const int fd)
 {
   fd_set fdset;
-  int ret;
   struct timeval timeout;
-  FD_ZERO( &fdset );
-  FD_SET( fd , &fdset );
+  FD_ZERO(&fdset);
+  FD_SET(fd, &fdset);
 
-  timeout.tv_sec = 0;
+  timeout.tv_sec  = 0;
   timeout.tv_usec = 0;
 
-  return select( fd+1 , &fdset , NULL , NULL , &timeout );
+  return select(fd + 1, &fdset, NULL, NULL, &timeout);
 }
 
-
-void SIGVerseTb3LaserDistanceSensorTeleopKey::moveBase(ros::Publisher &publisher, const double linear_x, const double angular_z)
+void SIGVerseTb3LaserDistanceSensorTeleopKey::moveBase(rclcpp::Publisher<geometry_msgs::msg::Twist>::SharedPtr &publisher, const double linear_x, const double angular_z)
 {
-  geometry_msgs::Twist twist;
-
+  geometry_msgs::msg::Twist twist;
   twist.linear.x  = linear_x;
   twist.linear.y  = 0.0;
   twist.linear.z  = 0.0;
@@ -78,9 +75,8 @@ void SIGVerseTb3LaserDistanceSensorTeleopKey::moveBase(ros::Publisher &publisher
   twist.angular.y = 0.0;
   twist.angular.z = angular_z;
 
-  publisher.publish(twist);
+  publisher->publish(twist);
 }
-
 
 void SIGVerseTb3LaserDistanceSensorTeleopKey::showHelp()
 {
@@ -100,7 +96,6 @@ void SIGVerseTb3LaserDistanceSensorTeleopKey::showHelp()
   puts("h: Show help");
 }
 
-
 void SIGVerseTb3LaserDistanceSensorTeleopKey::keyLoop(int argc, char** argv)
 {
   char c;
@@ -111,110 +106,99 @@ void SIGVerseTb3LaserDistanceSensorTeleopKey::keyLoop(int argc, char** argv)
   // get the console in raw mode
   int kfd = 0;
   struct termios cooked;
-
   struct termios raw;
+
   tcgetattr(kfd, &cooked);
   memcpy(&raw, &cooked, sizeof(struct termios));
-  raw.c_lflag &=~ (ICANON | ECHO);
+  raw.c_lflag &= ~(ICANON | ECHO);
   raw.c_cc[VEOL] = 1;
   raw.c_cc[VEOF] = 2;
   tcsetattr(kfd, TCSANOW, &raw);
   /////////////////////////////////////////////
+  
+  rclcpp::init(argc, argv);
 
-  ros::init(argc, argv, "tb3_lds_teleop_key", ros::init_options::NoSigintHandler);
+  auto node = rclcpp::Node::make_shared("tb3_lds_teleop_key");
 
-  ros::NodeHandle node_handle;
-
-  // Override the default ros sigint handler.
-  // This must be set after the first NodeHandle is created.
+  // Override the default sigint handler.
   signal(SIGINT, rosSigintHandler);
 
-  ros::Rate loop_rate(10);
+  auto pub_base_twist = node->create_publisher<geometry_msgs::msg::Twist>("/tb3/cmd_vel", 10);
 
-  ros::Publisher pub_base_twist = node_handle.advertise<geometry_msgs::Twist>("/tb3/cmd_vel", 10);
+  rclcpp::WallRate loop_rate(10);
 
+  auto logger = node->get_logger();
   sleep(2);
-
   showHelp();
 
-  while (ros::ok())
+  while (rclcpp::ok())
   {
-    if(canReceiveKey(kfd))
+    if (canReceiveKey(kfd))
     {
-      // get the next event from the keyboard
-      if((ret = read(kfd, &buf, sizeof(buf))) < 0)
+      if ((ret = read(kfd, &buf, sizeof(buf))) < 0)
       {
         perror("read():");
         exit(EXIT_FAILURE);
       }
 
-      c = buf[ret-1];
+      c = buf[ret - 1];
 
-      switch(c)
+      switch (c)
       {
         case KEYCODE_SPACE:
         {
-          ROS_DEBUG("Stop");
+          RCLCPP_DEBUG(logger, "Stop");
           moveBase(pub_base_twist, 0.0, 0.0);
           break;
         }
         case KEY_W:
         case KEYCODE_UP:
         {
-          ROS_DEBUG("Go Forward");
+          RCLCPP_DEBUG(logger, "Go Forward");
           moveBase(pub_base_twist, +LINEAR_VEL, 0.0);
           break;
         }
         case KEY_S:
         case KEYCODE_DOWN:
         {
-          ROS_DEBUG("Go Back");
+          RCLCPP_DEBUG(logger, "Go Back");
           moveBase(pub_base_twist, -LINEAR_VEL, 0.0);
           break;
         }
         case KEY_D:
         case KEYCODE_RIGHT:
         {
-          ROS_DEBUG("Turn Right");
+          RCLCPP_DEBUG(logger, "Turn Right");
           moveBase(pub_base_twist, 0.0, -ANGULAR_VEL);
           break;
         }
         case KEY_A:
         case KEYCODE_LEFT:
         {
-          ROS_DEBUG("Turn Left");
+          RCLCPP_DEBUG(logger, "Turn Left");
           moveBase(pub_base_twist, 0.0, +ANGULAR_VEL);
           break;
         }
         case KEY_H:
         {
-          ROS_DEBUG("Show Help");
+          RCLCPP_DEBUG(logger, "Show Help");
           showHelp();
           break;
         }
       }
     }
 
-    ros::spinOnce();
-
+    rclcpp::spin_some(node);
     loop_rate.sleep();
   }
 
-  /////////////////////////////////////////////
   // cooked mode
   tcsetattr(kfd, TCSANOW, &cooked);
-  /////////////////////////////////////////////
-
-  return;
 }
-
 
 int main(int argc, char** argv)
 {
   SIGVerseTb3LaserDistanceSensorTeleopKey teleop_key;
-
   teleop_key.keyLoop(argc, argv);
-
-  return(EXIT_SUCCESS);
+  return EXIT_SUCCESS;
 }
-
